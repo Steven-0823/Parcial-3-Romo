3ac69c9e73471904f424f94e6e565b55142ad0b2
Sebastian Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-05-21 08:25:06 -0500

    Controlador Reserva_Servicios

--- app/Http/Controllers/api/Reserva_ServiciosController.php
+++ app/Http/Controllers/api/Reserva_ServiciosController.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\ReservaServicios;
-
+use App\Models\Reservas;
+use App\Models\Servicios;
 
 class Reserva_ServiciosController extends Controller
 {
@@ -15,7 +16,13 @@ class Reserva_ServiciosController extends Controller
      */
     public function index()
     {
-        
+        $reserva_servicio = DB::table('reserva_servicios')
+            ->join('reservas', 'reserva_servicios.reserva_id', '=', 'reservas.id')
+            ->join('servicios', 'reserva_servicios.servicio_id', '=', 'servicios.id')
+            ->select('reserva_servicios.*', 'reservas.fecha_entrada', 'reservas_fecha_salida', 'reservas.estado', 'servicios.nombre as servicio_nombre', 'servicios.descripcion as servicio_descripcion', 'servicios.costo as servicio_costo')
+            ->get();
+
+        return response()->json(['reserva_servicios' => $reserva_servicios], 200);
     }
 
     /**
@@ -23,7 +30,14 @@ class Reserva_ServiciosController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $reserva_servicio = new Reserva_Servicio();
+        $reserva_servicio->reserva_id = $request->reserva_id;
+        $reserva_servicio->servicio_id = $request->servicio_id;
+        $reserva_servicio->fecha = $request->fecha;
+        $reserva_servicio->cantidad = $request->cantidad;
+        $reserva_servicio->save();
+
+        return response()->json(['reserva_servicio' => $reserva_servicio], 200);
     }
 
     /**
@@ -31,7 +45,9 @@ class Reserva_ServiciosController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $reserva_servicio = Reserva_Servicio::find($id);
+        return response()->json(['reserva_servicio' => $reserva_servicio], 200);
+
     }
 
     /**
@@ -39,7 +55,14 @@ class Reserva_ServiciosController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $reserva_servicio = Reserva_Servicio::find($id);
+        $reserva_servicio->reserva_id = $request->reserva_id;
+        $reserva_servicio->servicio_id = $request->servicio_id;
+        $reserva_servicio->fecha = $request->fecha;
+        $reserva_servicio->cantidad = $request->cantidad;
+        $reserva_servicio->save();
+
+        return response()->json(['reserva_servicio' => $reserva_servicio], 200);
     }
 
     /**
@@ -47,6 +70,9 @@ class Reserva_ServiciosController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $reserva_servicio = Reserva_Servicio::find($id);
+        $reserva_servicio->delete();
+
+        return response()->json(['reserva_servicio' => $reserva_servicio], 200);
     }
 }
